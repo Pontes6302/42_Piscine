8b24f395e9645be165aabe309ce9a2e5462a1871
Sivio joao Freitas pontes <sifreita@c1r14s5.42lisboa.com>
2021-11-22 03:12:28 +0000

    85%

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sifreita <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/12 18:26:48 by sifreita          #+#    #+#             */
+/*   Updated: 2021/08/12 18:26:50 by sifreita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	s;
+
+	s = 0;
+	i = 0;
+	while (src[s] != '\0')
+		s++;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (s);
+}
